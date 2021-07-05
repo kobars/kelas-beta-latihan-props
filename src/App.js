6094cd2e7bced29d28a62e048c2443ecb43b0a43
kobars <kobar.sept@gmail.com>
2021-07-05 22:19:39 +0700

    add context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
+import { createContext, useReducer } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/index";
-// import HalamanDua from "./pages/HalamanDua";
+import HalamanDua from "./pages/HalamanDua";
 // import NotFound from "./pages/NotFound";
 
+const initialStates = {
+  pokemonName: "BUKAN BULBASAUR",
+  pokemonUrl: "",
+};
+
+export const PokemonContext = createContext();
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "pokemon":
+      return { ...state, pokemonName: action.payload };
+    case "mentor":
+      return { ...state, pokemonName: "Kobar" };
+    default:
+      throw new Error();
+  }
+}
+
 const BasicExample = () => {
+  const [states, dispatch] = useReducer(reducer, initialStates);
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        {/* <Route exact path="/page-two">
-          <HalamanDua />
-        </Route> */}
-        {/* <Route path="*">
+    <PokemonContext.Provider value={{ states, dispatch }}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/page-two">
+            <HalamanDua />
+          </Route>
+          {/* <Route path="*">
           <NotFound />
         </Route> */}
-      </Switch>
-    </Router>
+        </Switch>
+      </Router>
+    </PokemonContext.Provider>
   );
 };
 
---
+++ src/pages/HalamanDua.js
@@ -0,0 +1,5 @@
+const HalamanDua = () => {
+  return <h1>Ganti di sini</h1>;
+};
+
+export default HalamanDua;
