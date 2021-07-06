9a9d56f624f91993d6b1e85cee1908bdd3e0305e
Ruri Wijaya <rury.wijaya@gmail.com>
2021-07-06 11:29:26 +0700

    belajar useContext

--- src/components/Comp5.js
+++ src/components/Comp5.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { PokemonContext } from "../App";
+
 const Comp5 = ({ pkn }) => {
+  const { states } = useContext(PokemonContext);
   return (
     <div className="mt-4">
       <h6>Component 5</h6>
-      <h6>isi di sini</h6>
+      <h6>{states.pokemonName}</h6>
     </div>
   );
 };
